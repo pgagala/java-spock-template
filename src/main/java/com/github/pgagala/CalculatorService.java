dac2e142bdcf5be261a7c2c5548c6a6021f5aa6c
Pawel Gagala <pawelgagala@gmail.com>
2022-11-30 05:16:19 +0100

    add example calculator service

--- src/main/java/com/github/pgagala/CalculatorService.java
+++ src/main/java/com/github/pgagala/CalculatorService.java
@@ -1,5 +1,21 @@
 package com.github.pgagala;
 
+import java.math.BigDecimal;
+
 public class CalculatorService {
 
+    public Number add(Number first, Number second) {
+        if (first == null || second == null) {
+            throw new IllegalArgumentException("null first or second");
+        }
+        if (first.getClass() != second.getClass()) {
+            throw new IllegalArgumentException("different class first and second");
+        }
+
+        return switch (first) {
+            case BigDecimal number -> number.add(new BigDecimal(second.toString()));
+            case Integer number -> number + second.intValue();
+            default -> throw new IllegalArgumentException("Number type not supported yet: " + first.getClass());
+        };
+    }
 }
--- src/test/groovy/com/github/pgagala/CalculatorServiceSpec.groovy
+++ src/test/groovy/com/github/pgagala/CalculatorServiceSpec.groovy
@@ -4,9 +4,38 @@ import spock.lang.Specification
 
 class CalculatorServiceSpec extends Specification {
 
-    def "Test"() {
-        expect:
-        throw new NoSuchMethodException("not implemented yet")
+    private CalculatorService calculatorService = new CalculatorService()
+
+    def "Sum should be added"() {
+        when:
+        def result = calculatorService.add(first, second)
+
+        then:
+        result == expectedResult
+
+        where:
+        first               | second               | expectedResult
+        1                   | 2                    | 3
+        0                   | 0                    | 0
+        5                   | 3                    | 8
+        4.0                 | 3.3                  | 7.3
+        new BigDecimal(4.5) | new BigDecimal(4.67) | new BigDecimal(9.17)
+    }
+
+    def "For invalid arguments exception should be thrown"() {
+        when:
+        calculatorService.add(first, second)
+
+        then:
+        def exc = thrown(IllegalArgumentException)
+        exc != null
+
+        where:
+        first | second
+        null  | 2
+        0     | null
+        null  | null
+        2     | new BigDecimal(2)
     }
 
 }
